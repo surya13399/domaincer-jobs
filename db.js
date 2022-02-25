9dfb7424724c6065e286316a51aae7a9da8c2ab4
Surya <85937537+surya13399@users.noreply.github.com>
2022-02-24 22:54:28 -0800

    Update db.js

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ dbConnect()
 async function dbConnect(){
 
      try {
-         await mongoose.connect('mongodb+srv://surya:surya123@cluster0.cemih.mongodb.net/sheyjobsudemy?retryWrites=true&w=majority' , {useNewUrlParser : true});
+         await mongoose.connect('mongodb+srv://surya:surya123@cluster0.cemih.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {useNewUrlParser : true});
          console.log('Mongo DB Connection success')
      } catch (error) {
          console.log('Mongo DB Connection failed')
